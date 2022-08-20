c969c08f37ba3dd80743618a651700a99264d84f
Kiko Vidal <vidaldiazf@gmail.com>
2022-08-20 18:51:35 +0200

    primera subida

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+from flask_cors import CORS
+
+app = flask.Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hola_mundo():
+    return 'hola kiko wapoooo'
+
+if __name__ == '__main__':
+    app.run()
